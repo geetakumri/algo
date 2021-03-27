ae28fe4eebdf095d6c553fd5ffdaa0b403abfd04
geetakumri <geet.choti1@gmail.com>
2021-03-27 22:13:02 +0530

    practice

--- leetcode/monotonic_array.py
+++ leetcode/monotonic_array.py
@@ -1,11 +1,15 @@
 #Write a Python Program to check if given array is Monotonic?
-class Solution :
+class Monotonic :
     def isMonotonic(self, A):
         return (all(A[i] <= A[i+1] for i in range(len(A) - 1)) or
                 all(A[i] >= A[i+1] for i in range(len(A) - 1)))
 
-lst = [4,8,9,6,4]
+lst = []
+size = int(input('Enter the size: '))
+print('Enter Elements')
+for i in range (0, size):
+    lst.append(int(input()))
 
-person = Solution()
+obj = Monotonic()
 
-print(person.isMonotonic(lst))
+print(obj.isMonotonic(lst))
